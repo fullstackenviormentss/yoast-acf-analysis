15d7d08a5ede8b2eb8bc0536d2dc748d742b1acb
Thomas Kräftner <thomas@kraftner.com>
2017-05-19 17:34:26 +0200

    only enqueue scripts where relevant

--- inc/class-frontend.php
+++ inc/class-frontend.php
@@ -22,33 +22,41 @@ class Yoast_ACF_Analysis_Frontend {
 	 */
 	public function enqueue_scripts() {
 
-		// Post page enqueue.
-		wp_enqueue_script(
-			'yoast-acf-analysis-post',
-			plugins_url( '/js/yoast-acf-analysis.js', YOAST_ACF_ANALYSIS_FILE ),
-			array(
-				'jquery',
-				'yoast-seo-post-scraper',
-				'underscore'
-			),
-			$this->plugin_data['Version']
-		);
-
-		// Term page enqueue.
-		wp_enqueue_script(
-			'yoast-acf-analysis-term',
-			plugins_url( '/js/yoast-acf-analysis.js', YOAST_ACF_ANALYSIS_FILE ),
-			array(
-				'jquery',
-				'yoast-seo-term-scraper',
-			),
-			$this->plugin_data['Version']
-		);
+		global $pagenow;
 
 		$config = Yoast_ACF_Analysis_Registry::instance()->get( 'config' );
 
-		wp_localize_script( 'yoast-acf-analysis-post', 'YoastACFAnalysisConfig', $config );
-		wp_localize_script( 'yoast-acf-analysis-term', 'YoastACFAnalysisConfig', $config );
+		// Post page enqueue.
+		if( 'post.php' === $pagenow || 'post-new.php' === $pagenow ){
+
+			wp_enqueue_script(
+				'yoast-acf-analysis-post',
+				plugins_url( '/js/yoast-acf-analysis.js', YOAST_ACF_ANALYSIS_FILE ),
+				array(
+					'jquery',
+					'yoast-seo-post-scraper',
+					'underscore'
+				),
+				$this->plugin_data['Version']
+			);
+
+			wp_localize_script( 'yoast-acf-analysis-post', 'YoastACFAnalysisConfig', $config );
+		}
+
+		if( 'term.php' === $pagenow ){
+			// Term page enqueue.
+			wp_enqueue_script(
+				'yoast-acf-analysis-term',
+				plugins_url( '/js/yoast-acf-analysis.js', YOAST_ACF_ANALYSIS_FILE ),
+				array(
+					'jquery',
+					'yoast-seo-term-scraper',
+				),
+				$this->plugin_data['Version']
+			);
+
+			wp_localize_script( 'yoast-acf-analysis-term', 'YoastACFAnalysisConfig', $config );
+		}
 
 	}
 
