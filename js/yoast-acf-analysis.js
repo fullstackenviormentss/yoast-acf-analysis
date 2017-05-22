682eca0b4487f4726b5540accfa24bd0127d5e42
Thomas Kräftner <thomas@kraftner.com>
2017-05-22 17:06:22 +0200

    also handle taxonomies displayed as checkboxes

--- js/yoast-acf-analysis.js
+++ js/yoast-acf-analysis.js
@@ -522,10 +522,30 @@ Scraper.prototype.scrape = function(fields){
             return field;
         }
 
-        var terms = _.pluck(field.$el.find('input').select2('data'), 'text');
+        var terms;
+
+        if( field.$el.find('.acf-taxonomy-field[data-type="multi_select"]').length > 0 ){
+
+            terms = _.pluck(
+                field.$el.find('.acf-taxonomy-field[data-type="multi_select"] input')
+                    .select2('data')
+                , 'text'
+            );
+
+        }else if( field.$el.find('.acf-taxonomy-field[data-type="checkbox"]').length > 0 ){
+
+            terms = _.pluck(
+                field.$el.find('.acf-taxonomy-field[data-type="checkbox"] input[type="checkbox"]:checked')
+                    .next(),
+                'textContent'
+            );
+
+        }
 
         if(terms.length>0){
             field.content = '<ul>\n<li>' + terms.join('</li>\n<li>') + '</li>\n</ul>';
+        }else{
+            field.content = '';
         }
 
         return field;
