7fe79429ca596a77785c377c473c1a62fb6505c9
Thomas Kräftner <thomas@kraftner.com>
2017-06-02 14:28:18 +0200

    automatically start and stop chromedriver, add lock file

--- tests/js/system/.env.example.js
+++ tests/js/system/.env.example.js
@@ -1,8 +1,16 @@
 module.exports = {
     "user"     : "wp",
     "password" : "wp",
+    before : function(done) {
+        chromedriver.start();
+        done();
+    },
+    after : function(done) {
+        chromedriver.stop();
+        done();
+    },
     beforeEach: function( browser, done ) {
         var page = browser.page.WordPressHelper();
         page.login();
     }
-}
+};
