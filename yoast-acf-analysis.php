73e7bef426be94697b70c8c727ee531b1d87def0
Thomas Kräftner <thomas@kraftner.com>
2017-06-29 17:10:45 +0200

    make bootstrap more readable

--- yoast-acf-analysis.php
+++ yoast-acf-analysis.php
@@ -26,15 +26,17 @@ if ( ! defined( 'YOAST_ACF_ANALYSIS_FILE' ) ) {
  * @see https://getcomposer.org/doc/00-intro.md#system-requirements
  */
 if ( version_compare( PHP_VERSION, '5.3.2', '<' ) && file_exists( YOAST_ACF_ANALYSIS_FILE . '/vendor/autoload.php' ) ) {
+
 	add_action(
 		'admin_notices',
 		create_function( '', "echo '<div class=\"error\"><p>" . __( 'Plugin Name requires PHP 5.3.2+ to function properly. Please upgrade PHP.', 'yoast-acf-analysis' ) . "</p></div>';" )
 	);
 	return;
-} else {
-	require dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload.php';
-}
 
-$yoast_acf_analysis = new Yoast_ACF_Analysis();
-$yoast_acf_analysis->init();
-unset( $yoast_acf_analysis );
+} else {
+
+	require dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload.php';
+
+	$yoast_acf_analysis = new Yoast_ACF_Analysis();
+	$yoast_acf_analysis->init();
+}
