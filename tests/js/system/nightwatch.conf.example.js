4c87e152d24e04b8135721061e8240331c144c2a
Thomas Kräftner <thomas@kraftner.com>
2017-07-03 13:21:46 +0200

    change default launch_url to match https://github.com/kraftner/yoast-acf-analysis-development

--- tests/js/system/nightwatch.conf.example.js
+++ tests/js/system/nightwatch.conf.example.js
@@ -1,4 +1,4 @@
 module.exports = (function(settings) {
-    settings.test_settings.default.launch_url = "http://site.vvv.dev";
+    settings.test_settings.default.launch_url = "http://yoast-acf-analysis.vvv.dev";
     return settings;
 })(require('./nightwatch.json'));
