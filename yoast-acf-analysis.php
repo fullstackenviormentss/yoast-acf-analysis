dbb342fe6cc5d58d81abcb0c54660ab020ddd94d
Thomas Kräftner <thomas@kraftner.com>
2017-07-04 17:56:34 +0200

    added xrstf/composer-php52 for PHP 5.2 support

--- yoast-acf-analysis.php
+++ yoast-acf-analysis.php
@@ -20,24 +20,9 @@ if ( ! defined( 'YOAST_ACF_ANALYSIS_FILE' ) ) {
 	define( 'YOAST_ACF_ANALYSIS_FILE', __FILE__ );
 }
 
-/**
- * Composer only supports PHP 5.3.2+
- *
- * @see https://getcomposer.org/doc/00-intro.md#system-requirements
- */
-if ( version_compare( PHP_VERSION, '5.3.2', '<' ) && file_exists( YOAST_ACF_ANALYSIS_FILE . '/vendor/autoload.php' ) ) {
+if ( file_exists( dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload_52.php' ) ) {
 
-	if ( is_admin() ) {
-		add_action(
-			'admin_notices',
-			create_function( '', "echo '<div class=\"error\"><p>" . __( 'Plugin Name requires PHP 5.3.2+ to function properly. Please upgrade PHP.', 'yoast-acf-analysis' ) . "</p></div>';" )
-		);
-	}
-	return;
-
-} elseif ( file_exists( dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload.php' ) ) {
-
-	require dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload.php';
+	require dirname( YOAST_ACF_ANALYSIS_FILE ) . '/vendor/autoload_52.php';
 
 	$yoast_acf_analysis = new Yoast_ACF_Analysis();
 	$yoast_acf_analysis->init();
