2f794b9539d4f0e77a0edf84e81a488c7c205e42
Jip Moors <jhp.moors@gmail.com>
2017-10-26 11:48:53 +0200

    Fix coding standards

--- inc/class-ac-yoast-seo-acf-content-analysis.php
+++ inc/class-ac-yoast-seo-acf-content-analysis.php
@@ -92,7 +92,7 @@ class AC_Yoast_SEO_ACF_Content_Analysis {
 	protected function register_config_filters() {
 		add_filter(
 			Yoast_ACF_Analysis_Facade::get_filter_name( 'scraper_config' ),
-			array( $this, 'filter_scraper_config')
+			array( $this, 'filter_scraper_config' )
 		);
 	}
 
