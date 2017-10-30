7ddb1f7d38a6ba1b9300fb65774170543d0a069b
Jip Moors <jhp.moors@gmail.com>
2017-10-30 14:31:08 +0100

    Remove irrelevant test
    Related code has changed in https://github.com/Yoast/yoast-acf-analysis/pull/104

--- tests/php/unit/MainTest.php
+++ tests/php/unit/MainTest.php
@@ -19,12 +19,6 @@ class MainTest extends \PHPUnit_Framework_TestCase {
 		$registry->add( 'config', 'Invalid Config' );
 
 		$testee = new \AC_Yoast_SEO_ACF_Content_Analysis();
-
-		Functions\expect('get_option')
-			->once()
-			->with('acf_version')
-			->andReturn(5);
-
 		$testee->boot();
 
 		$this->assertNotSame( 'Invalid Config', $registry->get( 'config' ) );
