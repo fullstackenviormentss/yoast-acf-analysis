7c3776b18d9dd689a6eb9cc05769857d7d26d9e8
Jip Moors <jhp.moors@gmail.com>
2017-10-31 15:24:28 +0100

    Add requirements for yoast eslint config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,31 @@
 /* global require, process */
-module.exports = function(grunt) {
-	'use strict';
-
-	require('time-grunt')(grunt);
+module.exports = function( grunt ) {
+	require( "time-grunt" )( grunt );
 
 	// Define project configuration
 	var project = {
 		paths: {
 			get config() {
-				return this.grunt + 'config/';
+				return this.grunt + "config/";
 			},
-			grunt: 'grunt/',
-			js: 'js/src/'
+			grunt: "grunt/",
+			js: "js/src/",
 		},
 		files: {
 			js: [
-				'js/src/**/*.js'
+				"js/src/**/*.js",
 			],
 			get config() {
-				return project.paths.config + '*.js';
+				return project.paths.config + "*.js";
 			},
-			grunt: 'Gruntfile.js'
+			grunt: "Gruntfile.js",
 		},
-		pkg: grunt.file.readJSON( 'package.json' )
+		pkg: grunt.file.readJSON( "package.json" ),
 	};
 
 	// Load Grunt configurations and tasks
-	require( 'load-grunt-config' )(grunt, {
-		configPath: require( 'path' ).join( process.cwd(), project.paths.config ),
-		data: project
-	});
+	require( "load-grunt-config" )( grunt, {
+		configPath: require( "path" ).join( process.cwd(), project.paths.config ),
+		data: project,
+	} );
 };
